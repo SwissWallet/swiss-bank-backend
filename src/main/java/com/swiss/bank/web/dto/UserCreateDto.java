64e196280224822bd9d2a6aa2163a3f8aac79dc0
unknown <pedrohenriquefdasilva14@gmail.com>
2024-10-09 21:57:01 -0300

    feat: create UserResposeDto

--- src/main/java/com/swiss/bank/web/dto/UserCreateDto.java
+++ src/main/java/com/swiss/bank/web/dto/UserCreateDto.java
@@ -1,2 +1,8 @@
-package com.swiss.bank.web.dto;public record UserCreateDto() {
+package com.swiss.bank.web.dto;
+
+public record UserCreateDto(String username,
+                            String name,
+                            String cpf,
+                            String phone,
+                            String password) {
 }
