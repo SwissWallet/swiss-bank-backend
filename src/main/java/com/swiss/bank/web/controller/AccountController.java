f25cf9686a3393bf20115d213aa615aba5b44f83
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-10-11 18:28:28 -0300

    feat: create AccountController

---
+++ src/main/java/com/swiss/bank/web/controller/AccountController.java
@@ -0,0 +1,16 @@
+package com.swiss.bank.web.controller;
+
+import com.swiss.bank.service.AccountService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+}
