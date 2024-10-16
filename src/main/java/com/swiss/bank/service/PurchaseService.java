4bf4f233ab28e4524dae30d5f5acdf7efe074a67
unknown <pedrohenriquefdasilva14@gmail.com>
2024-10-16 19:27:43 -0300

    feat: create PurchaseController

--- src/main/java/com/swiss/bank/service/PurchaseService.java
+++ src/main/java/com/swiss/bank/service/PurchaseService.java
@@ -1,7 +1,14 @@
 package com.swiss.bank.service;
 
+import com.swiss.bank.repository.IPurchaseRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PurchaseService {
+
+    private final IPurchaseRepository purchaseRepository;
+
+    public PurchaseService(IPurchaseRepository purchaseRepository) {
+        this.purchaseRepository = purchaseRepository;
+    }
 }
