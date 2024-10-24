43951df0b32e639235ad2b685af0a3057694ec20
unknown <pedrohenriquefdasilva14@gmail.com>
2024-10-24 19:24:56 -0300

    feat: create extract entity

--- src/main/java/com/swiss/bank/web/dto/PurchaseCreateDto.java
+++ src/main/java/com/swiss/bank/web/dto/PurchaseCreateDto.java
@@ -1,8 +1,5 @@
 package com.swiss.bank.web.dto;
 
-import com.swiss.bank.entity.Card;
-
-import java.time.LocalDateTime;
 
 public record PurchaseCreateDto(String username,
                                 String typePayment,
