b5a17d6469a4955ab3e24b37d3097462899bddda
unknown <pedrohenriquefdasilva14@gmail.com>
2024-10-24 19:41:58 -0300

    feat: change uri of extract

--- src/main/java/com/swiss/bank/web/controller/ExtractController.java
+++ src/main/java/com/swiss/bank/web/controller/ExtractController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v3/extracts")
+@RequestMapping("/api/v1/extracts")
 public class ExtractController {
 
     private final ExtractService extractService;
